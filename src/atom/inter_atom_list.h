8ae69f02bb288bfbadeb8e960a68b8db9a14c8e3
genshen <genshenchu@gmail.com>
2019-01-02 01:47:42 +0800

    feat(inter): add out-of-box check for inter atoms.
    and add test for out-of-box checking.

--- src/atom/inter_atom_list.h
+++ src/atom/inter_atom_list.h
@@ -17,6 +17,17 @@
 typedef std::list<AtomElement> _type_inter_list;
 typedef std::vector<std::vector<AtomElement *> > _type_inter_buf;
 
+namespace box {
+    typedef unsigned int _type_flag_32;
+    const _type_flag_32 IN_BOX = 0; // in box
+    const _type_flag_32 OUT_BOX_X_LITTER = 1; // out of box at x direction(in litter end)
+    const _type_flag_32 OUT_BOX_X_BIG = 1 << 1; // out of box at x direction(in big end)
+    const _type_flag_32 OUT_BOX_Y_LITTER = 1 << 2; // out of box at y direction(in litter end)
+    const _type_flag_32 OUT_BOX_Y_BIG = 1 << 3; // out of box at y direction(in big end)
+    const _type_flag_32 OUT_BOX_Z_LITTER = 1 << 4; // out of box at z direction(in litter end)
+    const _type_flag_32 OUT_BOX_Z_BIG = 1 << 5; // out of box at z direction(in big end)
+}
+
 /**
  * storing inter atoms
  */
@@ -48,6 +59,8 @@ public:
     void exchangeInter(Domain *p_domain);
 
     void borderInter(Domain *p_domain);
+
+    static const box::_type_flag_32 isOutBox(const AtomElement &src_atom, const Domain *p_domain);
 
     /**
      * pointer of element in atom_list (pointer of {@class AtomElement}).
@@ -83,11 +96,9 @@ private:
     /**
      * count the size of out-of-box inter atoms.
      * @param p_domain  pointer of simulation domain
-     * @param dimension  dimension of 3d. @param d values = {0,1,2}
-     * @param direction direction of LOW or HIGH.
-     * @return the number of out-of-box inter atoms.
+     * @param n_to_send  the number of out-of-box inter atoms will be stored in each dimension and each direction.
      */
-    unsigned long countExSendNum(Domain *p_domain, int dimension, int direction);
+    void countExSendNum(Domain *p_domain, int n_to_send[DIMENSION][2]);
 
     /**
      * If some inter atoms get out of box, those atom is no more in current dox of current processor.
@@ -99,7 +110,8 @@ private:
      * @param dimension dimension of 3d. @param d values = {0,1,2}
      * @param direction direction of LOW or HIGH.
      */
-    void packExInterToSend(Domain *p_domain, particledata *buf, int dimension, int direction);
+    void packExInterToSend(Domain *p_domain, particledata *buf, int dimension, int direction,
+                           box::_type_flag_32 excepted_flag[DIMENSION][2]);
 
     void unpackExInterRecv(int d, int n,
                            const double *lower, // p_domain->getMeasuredSubBoxLowerBounding(d)
---
+++ tests/unit/inter_atom_test.cpp
@@ -0,0 +1,61 @@
+//
+// Created by genshen on 2019-01-02.
+//
+
+#include <gtest/gtest.h>
+#include <domain/domain.h>
+#include <atom/inter_atom_list.h>
+
+Domain *buildLocalDomain(const int64_t local_space[DIMENSION],
+                         const int grid_size[DIMENSION],
+                         const int grid_coord[DIMENSION],
+                         const double lattice_const = 0.86,
+                         const double cutoff_radius_factor = 1.1421) {
+
+    const int64_t space[3] = {local_space[0] * grid_size[0],
+                              local_space[1] * grid_size[1],
+                              local_space[2] * grid_size[2]};
+    return Domain::Builder()
+            .setPhaseSpace(space)
+            .setCutoffRadius(cutoff_radius_factor)
+            .setLatticeConst(lattice_const)
+            .localBuild(grid_size, grid_coord);
+}
+
+TEST(inter_atom_test_isInBox_litter, inter_atom_Test) {
+    const double lattice_const = 0.86;
+    const double cutoff_factor = 1.1421;
+    const int grid_size[3] = {2, 2, 2};
+    const int grid_coord[3] = {0, 0, 0};
+    const int64_t local_space[3] = {50, 60, 72};
+    Domain *p_domain = buildLocalDomain(local_space, grid_size, grid_coord, lattice_const, cutoff_factor);
+
+    AtomElement atom;
+    // test in box
+    atom.x[0] = 10 * lattice_const;
+    atom.x[1] = 10 * lattice_const;
+    atom.x[2] = 10 * lattice_const;
+    auto flag1 = InterAtomList::isOutBox(atom, p_domain);
+    EXPECT_EQ(flag1, box::IN_BOX);
+
+    // test out box: x direction with litter end.
+    atom.x[0] = -10 * lattice_const;
+    atom.x[1] = 10 * lattice_const;
+    atom.x[2] = 10 * lattice_const;
+    auto flag2 = InterAtomList::isOutBox(atom, p_domain);
+    EXPECT_EQ(flag2, box::OUT_BOX_X_LITTER);
+
+    atom.x[0] = -0.01; // not out of box (the near atom is still on box).
+    atom.x[1] = 10 * lattice_const;
+    atom.x[2] = 10 * lattice_const;
+    auto flag3 = InterAtomList::isOutBox(atom, p_domain);
+    EXPECT_EQ(flag3, box::IN_BOX);
+
+    atom.x[0] = -lattice_const / 2 - 0.01; // not out of box (the near atom is still on box).
+    atom.x[1] = -lattice_const / 2 - 0.01;
+    atom.x[2] = -lattice_const / 2 - 0.01;
+    auto flag4 = InterAtomList::isOutBox(atom, p_domain);
+    EXPECT_EQ(flag4, box::OUT_BOX_X_LITTER | box::OUT_BOX_Y_LITTER | box::OUT_BOX_Z_LITTER);
+
+    delete p_domain;
+}
