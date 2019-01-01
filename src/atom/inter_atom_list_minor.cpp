db92860ef1799e1f055bd9b24b846e2f0fc3e4a3
genshen <genshenchu@gmail.com>
2019-01-02 01:50:32 +0800

    refactor(inter): use new added out-of-box checking method to exchange inter atoms with neighbour pro
    this is near lattice method.

--- src/atom/inter_atom_list_minor.cpp
+++ src/atom/inter_atom_list_minor.cpp
@@ -10,7 +10,7 @@
 
 void InterAtomList::exchangeInter(Domain *p_domain) {
     // 发送、接收数据缓冲区
-    int numPartsToSend[DIMENSION][2];
+    int num_parts_to_send[DIMENSION][2];
     int numPartsToRecv[DIMENSION][2];
     particledata *sendbuf[2];
     particledata *recvbuf[2];
@@ -22,18 +22,25 @@ void InterAtomList::exchangeInter(Domain *p_domain) {
     MPI_Request send_requests[DIMENSION][2];
     MPI_Request recv_requests[DIMENSION][2];
 
+    // pre calculate the number of send data
+    countExSendNum(p_domain, num_parts_to_send);
+
+    box::_type_flag_32 out_box_flags[DIMENSION][2] = {
+            {box::OUT_BOX_X_LITTER, box::OUT_BOX_X_BIG},
+            {box::OUT_BOX_Y_LITTER, box::OUT_BOX_Y_BIG},
+            {box::OUT_BOX_Z_LITTER, box::OUT_BOX_Z_BIG},
+    };
     int direction;
     // 找到要发送给邻居的原子
     for (unsigned short d = 0; d < DIMENSION; d++) {
         for (direction = LOWER; direction <= HIGHER; direction++) {
-            numPartsToSend[d][direction] = countExSendNum(p_domain, d, direction);
-            sendbuf[direction] = new particledata[numPartsToSend[d][direction]];
-            packExInterToSend(p_domain, sendbuf[direction], d, direction);
+            sendbuf[direction] = new particledata[num_parts_to_send[d][direction]];
+            packExInterToSend(p_domain, sendbuf[direction], d, direction, out_box_flags);
         }
 
         // 与上下邻居通信
         for (direction = LOWER; direction <= HIGHER; direction++) {
-            int numsend = numPartsToSend[d][direction];
+            int numsend = num_parts_to_send[d][direction];
             int numrecv;
 
             MPI_Isend(sendbuf[direction], numsend, mpi_types::_mpi_Particle_data,
@@ -69,78 +76,61 @@ void InterAtomList::exchangeInter(Domain *p_domain) {
     }
 }
 
-unsigned long InterAtomList::countExSendNum(Domain *p_domain, int dimension, int direction) {
-    unsigned long i = 0;
-    if (direction == 0) {
-        for (AtomElement &inter_ref :inter_list) {
-            // we assume that, a atom cannot cross 2 or more than 2 sub-boxes
-            if (inter_ref.x[dimension] < p_domain->meas_sub_box_region.low[dimension]) {
-                i++;
-            }
-        }
-    } else {
-        for (AtomElement &inter_ref :inter_list) {
-            if (inter_ref.x[dimension] >= p_domain->meas_sub_box_region.high[dimension]) {
-                i++;
-            }
+void InterAtomList::countExSendNum(Domain *p_domain, int n_to_send[DIMENSION][2]) {
+    box::_type_flag_32 flag;
+    // clear data.
+    for (int d = 0; d < DIMENSION; d++) {
+        for (int dir = LOWER; dir <= HIGHER; dir++) {
+            n_to_send[d][dir] = 0;
         }
     }
-    return i;
+
+    for (AtomElement &inter_ref :inter_list) {
+        // we assume that, a atom cannot cross 2 or more than 2 sub-boxes
+        flag = isOutBox(inter_ref, p_domain);
+        if (flag & box::OUT_BOX_X_LITTER) {
+            n_to_send[0][LOWER]++;
+        } else if (flag & box::OUT_BOX_X_BIG) {
+            n_to_send[0][HIGHER]++;
+        } else if (flag & box::OUT_BOX_Y_LITTER) {
+            n_to_send[1][LOWER]++;
+        } else if (flag & box::OUT_BOX_Y_BIG) {
+            n_to_send[1][HIGHER]++;
+        } else if (flag & box::OUT_BOX_Z_LITTER) {
+            n_to_send[2][LOWER]++;
+        } else if (flag & box::OUT_BOX_Z_BIG) {
+            n_to_send[2][HIGHER]++;
+        }
+    }
 }
 
-void InterAtomList::packExInterToSend(Domain *p_domain, particledata *buf, int dimension, int direction) {
-    if (direction == 0) {
-        unsigned long i = 0; // todo type
-        for (_type_inter_list::iterator inter_it = inter_list.begin();
-             inter_it != inter_list.end();) {
-            // we assume that, a atom cannot cross 2 or more than 2 sub-boxes
-            if ((*inter_it).x[dimension] < p_domain->meas_sub_box_region.low[dimension]) {
-                buf[i].id = inter_it->id;
-                buf[i].type = inter_it->type;
-                buf[i].r[0] = inter_it->x[0];
-                buf[i].r[1] = inter_it->x[1];
-                buf[i].r[2] = inter_it->x[2];
-                buf[i].v[0] = inter_it->v[0];
-                buf[i].v[1] = inter_it->v[1];
-                buf[i].v[2] = inter_it->v[2];
-                // remove the inter atom.
-                // exchange the atom at end of vector to the position of atom (inter[j]) te be removed .
-                inter_it = inter_list.erase(inter_it);
-                nlocalinter--;
-                i++;
-            } else {
-                inter_it++;
-            }
-        }
-    } else {
-        unsigned long i = 0;
-        for (_type_inter_list::iterator inter_it = inter_list.begin();
-             inter_it != inter_list.end();) {
-            // we assume that, a atom cannot cross 2 or more than 2 sub-boxes
-            if (inter_it->x[dimension] >= p_domain->meas_sub_box_region.high[dimension]) {
-                buf[i].id = inter_it->id;
-                buf[i].type = inter_it->type;
-                buf[i].r[0] = inter_it->x[0];
-                buf[i].r[1] = inter_it->x[1];
-                buf[i].r[2] = inter_it->x[2];
-                buf[i].v[0] = inter_it->v[0];
-                buf[i].v[1] = inter_it->v[1];
-                buf[i].v[2] = inter_it->v[2];
-                // remove the inter atom.
-                // exchange the atom at end of vector to the position of atom (inter[j]) te be removed .
-                inter_it = inter_list.erase(inter_it);
-                nlocalinter--;
-                i++;
-            } else {
-                inter_it++;
-            }
+void InterAtomList::packExInterToSend(Domain *p_domain, particledata *buf, int dimension, int direction,
+                                      box::_type_flag_32 excepted_flag[DIMENSION][2]) {
+    unsigned long i = 0; // todo type
+    for (_type_inter_list::iterator inter_it = inter_list.begin(); inter_it != inter_list.end();) {
+        // we assume that, a atom cannot cross 2 or more than 2 sub-boxes
+        if (isOutBox(*inter_it, p_domain) & excepted_flag[dimension][direction]) {
+            buf[i].id = inter_it->id;
+            buf[i].type = inter_it->type;
+            buf[i].r[0] = inter_it->x[0];
+            buf[i].r[1] = inter_it->x[1];
+            buf[i].r[2] = inter_it->x[2];
+            buf[i].v[0] = inter_it->v[0];
+            buf[i].v[1] = inter_it->v[1];
+            buf[i].v[2] = inter_it->v[2];
+            // remove the inter atom.
+            // exchange the atom at end of vector to the position of atom (inter[j]) te be removed .
+            inter_it = inter_list.erase(inter_it);
+            nlocalinter--;
+            i++;
+        } else {
+            inter_it++;
         }
     }
 }
 
 void InterAtomList::unpackExInterRecv(int d, int n, const double *lower, const double *upper, particledata *buf) {
     AtomElement atom;
-    atom_type::atom_type type;
     for (int i = 0; i < n; i++) {
         atom.id = buf[i].id;
         atom.type = buf[i].type;
