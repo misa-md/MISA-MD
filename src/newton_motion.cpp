4d51861989f1d24e0c7c82c8a83a36a9e6f711bf
genshen <genshenchu@gmail.com>
2019-05-10 23:27:12 +0800

    refactor(motion): remove addtional function calling and redundant intermediate variables.
    remove addtional function calling: computeFirst and computeSecond; and remove redundant intermediate
    variables or code such as atom type and for loops.

--- src/newton_motion.cpp
+++ src/newton_motion.cpp
@@ -28,15 +28,6 @@ void NewtonMotion::preComputeDtInv2m() {
 }
 
 void NewtonMotion::firststep(AtomList *atom_list, InterAtomList *inter_atom_list) {
-
-    computeFirst(atom_list, inter_atom_list);
-}
-
-void NewtonMotion::secondstep(AtomList *atom_list, InterAtomList *inter_atom_list) {
-    computeSecond(atom_list, inter_atom_list);
-}
-
-void NewtonMotion::computeFirst(AtomList *atom_list, InterAtomList *inter_atom_list) {
     double &dt = _timestepLength;
     //本地晶格点上的原子求解运动方程第一步
     atom_list->foreachSubBoxAtom(
@@ -55,7 +46,6 @@ void NewtonMotion::computeFirst(AtomList *atom_list, InterAtomList *inter_atom_l
     //本地间隙原子求解运动方程第一步
     atom_type::atom_type _type;
     for (AtomElement &inter_ref :inter_atom_list->inter_list) {
-//    for (int i = 0; i < inter_atom_list->nlocalinter; i++) {
         for (unsigned short d = 0; d < 3; ++d) {
             _type = inter_ref.type;
             inter_ref.v[d] = inter_ref.v[d] + dt_inv_m[_type] * inter_ref.f[d];
@@ -64,25 +54,21 @@ void NewtonMotion::computeFirst(AtomList *atom_list, InterAtomList *inter_atom_l
     }
 }
 
-
-void NewtonMotion::computeSecond(AtomList *atom_list, InterAtomList *inter_atom_list) {
+void NewtonMotion::secondstep(AtomList *atom_list, InterAtomList *inter_atom_list) {
     //本地晶格点上的原子求解运动方程第二步
     atom_list->foreachSubBoxAtom(
             [=](AtomElement &_atom_ref) {
-                // fixme, excluding InterElement. (add if: isInterElement)
-                for (unsigned short d = 0; d < DIMENSION; ++d) {
-                    _atom_ref.v[d] += dt_inv_m[_atom_ref.type] * _atom_ref.f[d];
+                if (!_atom_ref.isInterElement()) {
+                    _atom_ref.v[0] += dt_inv_m[_atom_ref.type] * _atom_ref.f[0];
+                    _atom_ref.v[1] += dt_inv_m[_atom_ref.type] * _atom_ref.f[1];
+                    _atom_ref.v[2] += dt_inv_m[_atom_ref.type] * _atom_ref.f[2];
                 }
             }
     );
     //本地间隙原子求解运动方程第二步
-    atom_type::atom_type _type;
     for (AtomElement &inter_ref :inter_atom_list->inter_list) {
-//    for (int i = 0; i < inter_atom_list->nlocalinter; i++) {
-        for (unsigned short d = 0; d < 3; ++d) {
-            _type = inter_ref.type;
-            inter_ref.v[d] =
-                    inter_ref.v[d] + dt_inv_m[_type] * inter_ref.f[d];
-        }
+        inter_ref.v[0] += dt_inv_m[inter_ref.type] * inter_ref.f[0];
+        inter_ref.v[1] += dt_inv_m[inter_ref.type] * inter_ref.f[1];
+        inter_ref.v[2] += dt_inv_m[inter_ref.type] * inter_ref.f[2];
     }
 }
