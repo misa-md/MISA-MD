a12c5e12592da4444ffbe11e3209206a83efd1ee
genshen <genshenchu@gmail.com>
2020-04-26 21:39:21 +0800

    build(cmake): add `MD_` prefix to variable names of cmake options
    BREAKING CHANGE: when performing cmake configuration, the option names have `MD_` prefix now
    (e.g. change 'SUNWAY_ARCH_ENABLE_FLAG' to `MD_SUNWAY_ARCH_ENABLE_FLAG`).

--- config.cmake
+++ config.cmake
@@ -5,19 +5,19 @@
 #set(CMAKE_C_COMPILER mpicc -cc=gcc -cxx=g++)
 #set(CMAKE_CXX_COMPILER mpicxx -cc=gcc -cxx=g++)
 
-option(OpenMP_ENABLE_FLAG "Use OpenMP" OFF) #change this flag to OFF to disable OpenMP
-option(MPI_ENABLE_FLAG "Use MPI library" ON) #change this flag to false to disable mpi
-option(TEST_ENABLE_FLAG "Enable test" ON) # enable test
-option(TEST_MPI_ENABLE_FLAG "Enable MPI in test" ON) # enable mpi in test, its value depends on option MPI_ENABLE_FLAG.
-option(TOOLS_BUILD_ENABLE_FLAG "Enable tools building" ON) # enable tools building (in tools directory) binary.(tools example: convert simulation result binary file to text file)
+option(MD_OpenMP_ENABLE_FLAG "Use OpenMP" OFF) #change this flag to OFF to disable OpenMP
+option(MD_MPI_ENABLE_FLAG "Use MPI library" ON) #change this flag to false to disable mpi
+option(MD_TEST_ENABLE_FLAG "Enable test" ON) # enable test
+option(MD_TEST_MPI_ENABLE_FLAG "Enable MPI in test" ON) # enable mpi in test, its value depends on option MPI_ENABLE_FLAG.
+option(MD_TOOLS_BUILD_ENABLE_FLAG "Enable tools building" ON) # enable tools building (in tools directory) binary.(tools example: convert simulation result binary file to text file)
 
-## architecture ralated values (only used in src/arch dir).
-option(SUNWAY_ARCH_ENABLE_FLAG "Enable sunway athread" OFF) # enable sunway athread if its running on sunway system.
-option(CUDA_ARCH_ENABLE_FLAG "Enable GPU using CUDA" OFF) # enable GPU if its running on nvidia GPU.
-option(HIP_ARCH_ENABLE_FLAG "Enable DCU using HIP" OFF) # enable DCU if its running on sugon DCU.
-set(SUNWAY_ARCH_SRC_PATH "" CACHE PATH "Source files directory of sunway architecture") # source file directory of sunway arch code.
-set(CUDA_ARCH_SRC_PATH "" CACHE PATH "Source directory of CUDA architecture code") # source file directory of cuda arch code.
-set(HIP_ARCH_SRC_PATH "" CACHE PATH "Source directory of HIP architecture code") # source file directory of hip arch code.
+## architecture related values (only used in src/arch dir).
+option(MD_SUNWAY_ARCH_ENABLE_FLAG "Enable sunway athread" OFF) # enable sunway athread if its running on sunway system.
+option(MD_CUDA_ARCH_ENABLE_FLAG "Enable GPU using CUDA" OFF) # enable GPU if its running on nvidia GPU.
+option(MD_HIP_ARCH_ENABLE_FLAG "Enable DCU using HIP" OFF) # enable DCU if its running on sugon DCU.
+set(MD_SUNWAY_ARCH_SRC_PATH "" CACHE PATH "Source files directory of sunway architecture") # source file directory of sunway arch code.
+set(MD_CUDA_ARCH_SRC_PATH "" CACHE PATH "Source directory of CUDA architecture code") # source file directory of cuda arch code.
+set(MD_HIP_ARCH_SRC_PATH "" CACHE PATH "Source directory of HIP architecture code") # source file directory of hip arch code.
 
 set(MD_RAND "MT" CACHE STRING "random number generating algorithm") # random number generating
 # options are:
