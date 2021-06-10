9bcd85b648e1b4ce948e0c074f7a224a906ee94c
genshen <genshenchu@gmail.com>
2021-06-10 15:29:13 +0800

    refactor(random): make all rng engine share the same implementation of generating new random number

--- src/utils/random/rand_generators.h
+++ src/utils/random/rand_generators.h
@@ -14,80 +14,41 @@
 
 namespace md_rand {
 #ifdef RAND_LCG
-    extern std::minstd_rand rng;
-
-    /**
-     * set seed for rng
-     */
-    void seed(const uint32_t seed);
-
-    /**
-     * \brief returns a uint32_t random type, between \param low to \param high.
-     */
-    uint32_t rand32();
+    typedef std::minstd_rand type_rng;
 #endif
 
 #ifdef RAND_MT
-    extern std::mt19937 rng;
-
-    /**
-     * set seed for rng
-     */
-    void seed(const uint32_t seed);
-
-    /**
-     * \brief returns a uint32_t random type, between \param low to \param high.
-     */
-    uint32_t rand32();
-
+    typedef std::mt19937 type_rng;
 #endif
 
 #ifdef RAND_STC
-    extern std::ranlux24 rng;
-
-    /**
-     * set seed for rng
-     */
-    void seed(const uint32_t seed);
-
-    /**
-     * \brief returns a uint32_t random type, between \param low to \param high.
-     */
-    uint32_t rand32();
+    typedef std::ranlux24 type_rng;
 #endif
 
 #ifdef RAND_XOSHIRO
     // return type is uint32_t for 128;
     // return type is uint64_t for 256;
-    extern util::random::xoroshiro128_plus rng;
-
-    /**
-     * set seed for rng
-     */
-    void seed(const uint32_t seed);
-
-    /**
-     * \brief returns a uint32_t random type, between \param low to \param high.
-     */
-    uint32_t rand32();
+    typedef util::random::xoroshiro128_plus type_rng;
 #endif
 
 #ifdef RAND_LEGACY
-    extern LegacyRand rng;
-
-    /**
-     * set seed for rng
-     */
-    void seed(const uint32_t seed);
-
-    /**
-     * \brief returns a uint32_t random type, between \param low to \param high.
-     */
-    uint32_t rand32();
+    typedef LegacyRand type_rng;
 #endif
 
 #ifdef RAND_LINUX_REAL
 #endif
+
+    extern type_rng rng;
+
+    /**
+     * set seed for rng
+     */
+    void seed(const uint32_t seed);
+
+    /**
+     * \brief returns a uint32_t random type, between \param low to \param high.
+     */
+    uint32_t rand32();
 }
 
 #endif //MISA_KMC_RAND_GENERATOR_H
